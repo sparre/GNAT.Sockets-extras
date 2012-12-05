93a95c81ccdcaf311d7c07213c779ffe89ae7021
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2012-12-05 12:31:20 +0100

    Explicit license information.

--- gnat-sockets-convenience.ads
+++ gnat-sockets-convenience.ads
@@ -1,3 +1,12 @@
+--  The Beer-Ware License (revision 42)
+--
+--  Jacob Sparre Andersen <jacob@jacob-sparre.dk> wrote this. As long as you
+--  retain this notice you can do whatever you want with this stuff. If we meet
+--  some day, and you think this stuff is worth it, you can buy me a beer in
+--  return.
+--
+--  Jacob Sparre Andersen
+
 package GNAT.Sockets.Convenience is
    subtype IP_Address_Type is Inet_Addr_Type;
 
